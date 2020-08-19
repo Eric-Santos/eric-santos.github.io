1a7c8340163da303d17ba88d22b5352123adeb80
eric <eric355402@yahoo.com>
2020-08-18 18:02:16 -0600

    Updates

---
+++ precache-manifest.39b8323350335630170580ae27b83f09.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55613873ced3c7016b4dbd9cfee567bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca4dc9aa968ce57e99e3",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cc539df475651e07c4c1",
+    "url": "/static/js/2.630d8937.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.630d8937.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca4dc9aa968ce57e99e3",
+    "url": "/static/js/main.5fd69493.chunk.js"
+  },
+  {
+    "revision": "273294a3757ec2956fe1",
+    "url": "/static/js/runtime-main.0d578fb4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
